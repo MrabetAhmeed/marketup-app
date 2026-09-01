3da44d9c2fefbba670ddc407cde8ee8c064c16cc
mrabet.ahmeed@mail.com <mrabet.ahmeed@gmail.com>
2026-09-01 12:13:40 +0100

    feat: ajout favicon.png sur toutes les pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "vivasky.media",
   description: "La plateforme digitale des entreprises tunisiennes",
+  icons: { icon: "/favicon.png" },
 };
 
 export default function RootLayout({
